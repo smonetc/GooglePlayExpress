2e61e3f41daa8302e6930320a716ad811c91dc2a
Monet Cooper <smomoc@monets-mbp-2.lan>
2020-10-08 07:09:23 -0400

    test added all working

--- test/app.test.js
+++ test/app.test.js
@@ -19,6 +19,50 @@ describe('GET /apps', () => {
         return supertest(app)
         .get('/apps')
         .query({sort: 'Whoops'})
-        .expect(400,'Sort must be rating or app')
+        .expect(400,'Sort must be one of rating or app')
+    })
+    it('should sort by rating', () => {
+        return supertest(app)
+        .get('/apps')
+        .query({sort:'Rating'})
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => {
+            expect(res.body).to.be.an('array')
+            let sorted = true
+            let i = 0
+            while(i < res.body.length - 1){
+                const rateAtI = res.body[i]
+                const rateAtIPlus1 = res.body[i + 1]
+                if(rateAtIPlus1.Rating < rateAtI.Rating){
+                    sorted = false
+                    break
+                }
+                i++
+            }
+            expect(sorted).to.be.true
+        })
+    })
+    it('should sort by app', () => {
+        return supertest(app)
+        .get('/apps')
+        .query({sort:'App'})
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => {
+            expect(res.body).to.be.an('array')
+            let sorted = true
+            let i = 0
+            while(i < res.body.length - 1){
+                const rateAtI = res.body[i]
+                const rateAtIPlus1 = res.body[i + 1]
+                if(rateAtIPlus1.App < rateAtI.App){
+                    sorted = false
+                    break
+                }
+                i++
+            }
+            expect(sorted).to.be.true
+        })
     })
 })
